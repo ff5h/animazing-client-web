2b1544e9fffd8a4b9530c8c92ccfc68b6edafe3c
RYBKIN <vdj.manka@gmail.com>
2023-02-23 17:04:11 +0200

    Make Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import HomePage from './pages/HomePage/Homepage';
+import HomePage from './pages/HomePage/HomePage';
+
+import '~/App.scss';
 
 const App = (): JSX.Element => {
   return (
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CategoryWrapper from '~/components/CategoryWrapper/CategoryWrapper';
+import Header from '~/components/Header/Header';
 
 import styles from './HomePage.module.scss';
 
@@ -8,6 +9,7 @@ type HomePageProps = {};
 const HomePage = ({}: HomePageProps): JSX.Element => {
   return (
     <div className={styles.HomePage}>
+      <Header />
       <CategoryWrapper />
     </div>
   );
