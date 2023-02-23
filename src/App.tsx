0e604a7d4c89f766e2c09076ea4448359fd9d7d3
Diancent <artem.korol.jet@gmail.com>
2023-02-23 17:32:29 +0200

    Fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { HomePage } from './pages/HomePage/HomePage.module.scss';
-
 import '~/App.scss';
+
+import HomePage from './pages/HomePage/HomePage';
 
 const App = (): JSX.Element => {
   return (
