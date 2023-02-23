436a806412b2ee0dc048e4e9a4fe75d400bef783
RYBKIN <vdj.manka@gmail.com>
2023-02-23 17:30:23 +0200

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import '~/App.scss';
 import { HomePage } from './pages/HomePage/HomePage.module.scss';
+
+import '~/App.scss';
 
 const App = (): JSX.Element => {
   return (
